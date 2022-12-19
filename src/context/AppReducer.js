6a7bbc0c2f27a8dc7159068511632ead38eda650
Anjalika <anjalika.agarwal@gmail.com>
2022-12-19 18:15:17 +0530

    1st commit

---
+++ src/context/AppReducer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { GlobalContext } from './GlobalState';
+export default (state, action) => {
+    switch (action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(item => item.id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [action.payload,...state.transactions]
+            }
+        default:
+            return state;
+    }
+}
